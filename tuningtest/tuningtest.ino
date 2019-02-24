d14bb55b60d0d7db23701db624646dd830de4c8b
macktang <macktang@gmail.com>
2019-02-24 08:09:23 -0700

    add Kp Kd writing routine

--- tuningtest/tuningtest.ino
+++ tuningtest/tuningtest.ino
@@ -1,6 +1,7 @@
 float Kp;
 float Kd;
 //float Ki;
+float q = 0.1; //value to increment Kp and Kd by
 
 void setup() {
   // put your setup code here, to run once:
@@ -12,47 +13,68 @@ void loop() {
   if (Serial.available()) {
     char c = Serial.read();
 
-    if(c =='r'){
-      readTuning();
-    }
+    if(c =='r'){readTuning();}
 
-    if (c == 'm'){
-      Serial.println("motor running");
-    }
+    if (c == 'm'){Serial.println("motor running");}
     
     if (c == 't'){
       Serial.flush();
       delay(10);
-
       tuningRoutine();
+    }
 
+    if (c == 'w'){
+      Serial.flush();
+      delay(10);
+      writeRoutine();
+    }
+    
+  }
+}
+
+void tuningRoutine(){  
+  if(Serial.available()){
+    Serial.print("tuning mode active...");                
+    char param = Serial.read();
+    char cmd = Serial.read();
+    switch(param){
+      case 'p':
+        if(cmd=='+'){Kp+=q; Serial.print("Kp increased to "); Serial.println(Kp,8);}
+        if(cmd=='-'){Kp-=q; Serial.print("Kp decreased to "); Serial.println(Kp,8);}
+        break;
+      case 'd':
+        if(cmd=='+'){Kd+=q; Serial.print("Kd increased to "); Serial.println(Kd,8);}
+        if(cmd=='-'){Kd-=q; Serial.print("Kd decreased to "); Serial.println(Kd,8);}
+        break;
     }
   }
 }
 
-void tuningRoutine()
-{  
-  if(Serial.available())
-    {
-      Serial.print("tuning mode active...");                
-      char param = Serial.read();
-      char cmd = Serial.read();
-      switch(param){
-        case 'p':
-          if(cmd=='+'){Kp+=1; Serial.print("Kp increased to "); Serial.println(Kp);}
-          if(cmd=='-'){Kp-=1; Serial.print("Kp decreased to "); Serial.println(Kp);}
-          break;
-        case 'd':
-          if(cmd='+'){Kd+=1; Serial.print("Kd incresed to "); Serial.println(Kd);}
-          if(cmd=='-'){Kd-=1; Serial.print("Kd decreased to "); Serial.println(Kd);}
-          break;
-      }
+void writeRoutine(){  
+  if(Serial.available()){
+    Serial.print("pid parameter writing mode active...");                
+    char param = Serial.read();
+    String s1 = Serial.readString();
+
+    switch(param){
+      case 'p':
+        Kp = s1.toFloat();
+        Serial.print("Kp set to: "); Serial.println(Kp,8);
+        break;
+      case 'd':
+        Kd = s1.toFloat();
+        Serial.print("Kd set to: "); Serial.println(Kd,8);
+        break;
+      case 'q':
+        q = s1.toFloat();
+        Serial.print("q (increment value) set to: "); Serial.println(q,8);
+        break;
     }
+  }
 }
 
-void readTuning()
-{
+void readTuning(){
   Serial.print("Kp:");    Serial.print(Kp,8);
-//  Serial.print("   Ki:"); Serial.print(Ki,8);
+  Serial.print("   q:"); Serial.print(q,8);
   Serial.print("   Kd:"); Serial.println(Kd,8);
 }
